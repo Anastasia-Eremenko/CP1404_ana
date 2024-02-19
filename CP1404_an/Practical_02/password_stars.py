7bebcef3352a546a2c9e1478f1e2ae4236196368
annabeanz <anastasia.ereme0307@gmail.com>
2024-02-19 13:35:07 +0800

    Add password check program

--- CP1404_an/Practical_02/password_stars.py
+++ CP1404_an/Practical_02/password_stars.py
@@ -5,9 +5,22 @@ Paper code for a password stars
 
 MINIMUM_LENGTH = 4
 
-passowrd = str(input("Enter your passowrd>> "))
-if len(passowrd) < MINIMUM_LENGTH:
-    print("Invalid password")
-else:
+def main():
+    passowrd = get_passowrd()
+    print_stars(passowrd)
+
+
+def print_stars(passowrd):
     for i in range(len(passowrd)):
         print("*", end="")
+
+
+def get_passowrd():
+    passowrd = str(input("Enter your passowrd>> "))
+    if len(passowrd) < MINIMUM_LENGTH:
+        print("Invalid password")
+        passowrd = get_passowrd()
+    return passowrd
+
+
+main()
