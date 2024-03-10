b36e02f2013e15a5240cd53cdf07cde2fce774d0
annabeanz <anastasia.ereme0307@gmail.com>
2024-03-10 21:10:01 +0800

    prac 4 list_exercises.py completed

--- CP1404_an/Practical_04/list_exercises.py
+++ CP1404_an/Practical_04/list_exercises.py
@@ -1,8 +1,11 @@
 """
 Anastasia Eremenko
 CP1404 - Practical
-Prompting the user for numbers, displaying number analysis
+Basic list operations and Woefully inadequate security checker
 """
+
+# Basic list operations
+
 QUANTITY_OF_NUMBERS = 5
 numbers = []
 
@@ -13,4 +16,36 @@ while len(numbers) < QUANTITY_OF_NUMBERS:
     except ValueError:
         print("It has to be a number")
 print(f"The first number is {numbers[0]}\nThe last number is {numbers[-1]}\nThe smallest number is {min(numbers)}\n"
-      f"The largest number is {max(numbers)}\nThe average of the numbers is {sum(numbers)/len(numbers)}")
+      f"The largest number is {max(numbers)}\nThe average of the numbers is {sum(numbers) / len(numbers)}")
+
+"""
+
+
+"""
+
+# Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+
+def main():
+    """A program that grants access based on valid usernames"""
+    while True:  # program modified to repeatedly ask, until a correct user is entered
+        name = input("username: ")
+        if check_if_valid_user(name):
+            print("Access granted")
+            break
+        else:
+            print("Access denied")
+
+
+def check_if_valid_user(name):
+    """Check if the username is valid"""
+    access = False
+    for valid_user in usernames:
+        if valid_user == name:
+            access = True
+    return access
+
+
+main()
