79873ae810757e66895f410141fe9503fdcbe9f0
annabeanz <anastasia.ereme0307@gmail.com>
2024-03-10 20:22:53 +0800

    prac 4 subject reader finished

--- CP1404_an/Practical_04/subject_reader.py
+++ CP1404_an/Practical_04/subject_reader.py
@@ -15,6 +15,7 @@ def main():
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    all_parts = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,8 +24,16 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        all_parts.append(parts)
         print("----------")
+    print(all_parts)  # Question 2 - display a nested list of all the names
+    display_formated_part(all_parts) # Question 3 - Displays formatted parts
     input_file.close()
+
+def display_formated_part(all_parts):
+    """Displays a formatted string for each set of parts"""
+    for part in all_parts:
+        print(f"{part[0]} is taught by {part[1]:<12} and has {part[2]:>3} students")
 
 
 main()
