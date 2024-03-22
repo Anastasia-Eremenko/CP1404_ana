136d932de1d8a4d7de1c1ad21e6655ec0609d0a0
annabeanz <anastasia.ereme0307@gmail.com>
2024-03-22 12:25:00 +0800

    adjusted prac 6 guitar exercise

--- CP1404_an/Practical_06/guitars.py
+++ CP1404_an/Practical_06/guitars.py
@@ -3,7 +3,6 @@ Prac 06 CP1404 - Guitar class operations :test file
 Expected time: 20min
 Actual time: 30min
 """
-
 from Practical_06.guitar import Guitar
 
 TITLE = "My guitars!"
@@ -13,21 +12,28 @@ guitars = []
 def main():
     """Program that gets and processes user guitar info based on Guitar Class"""
     print(TITLE)
-    guitar_name = input("Name: ")
-    while guitar_name != "":
-        guitar_year = int(get_valid_number("Year: "))
+    while True:
+        guitar_name = input("Name: ")
+        if guitar_name == "":
+            break
+        guitar_year = get_valid_number("Year: ")
         guitar_cost = get_valid_number("Cost: ")
         guitar = Guitar(guitar_name, guitar_year, guitar_cost)
         print(f"{guitar} added")
-        guitar_name = input("Name: ")
         guitars.append(guitar)
 
-    for i, guitar in enumerate(guitars):
+    print_guitars()
+
+
+def print_guitars():
+    """Prints the list of guitars with their details."""
+    for i, guitar in enumerate(guitars, 1):
         category = "(Vintage)" if guitar.is_vintage() else ""
-        print(f"{i + 1} {guitar} {category}")
+        print(f"Guitar {i}: {guitar.name:<20} ({guitar.year}), worth ${guitar.cost:10,.2f}{category}")
 
 
 def get_valid_number(prompt):
+    """Ensures the value entered is a number and is above 0"""
     while True:
         try:
             number = float(input(prompt))
